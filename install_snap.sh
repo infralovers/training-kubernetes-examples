ac8c8496617f8c9c90c75e574d1d31c9a0800da2
Edmund Haselwanter <ehaselwanter@infralovers.com>
2019-10-16 09:18:35 +0200

    Create install_snap.sh

---
+++ install_snap.sh
@@ -0,0 +1,7 @@
+## snap
+sudo yum install epel-release -y
+sudo yum install snapd -y
+sudo systemctl enable --now snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
+sudo systemctl start snapd
+sudo systemctl status snapd.socket
