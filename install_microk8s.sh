e293eb4705885a5f090a1c8c79b42a97bdac8d10
Edmund Haselwanter <ehaselwanter@infralovers.com>
2019-11-03 16:33:44 +0100

    fix init scripts

--- install_microk8s.sh
+++ install_microk8s.sh
@@ -1,6 +1,10 @@
 
-
 ## microk8s
+sudo systemctl start snapd
+sudo systemctl status snapd.socket
+
 sudo snap install microk8s --channel=1.15/stable --classic
 sudo usermod -a -G microk8s centos
 sudo snap alias microk8s.kubectl kubectl
+
+microk8s.status --wait-ready
--- install_snap.sh
+++ install_snap.sh
@@ -3,5 +3,3 @@ sudo yum install epel-release -y
 sudo yum install snapd -y
 sudo systemctl enable --now snapd.socket
 sudo ln -s /var/lib/snapd/snap /snap
-sudo systemctl start snapd
-sudo systemctl status snapd.socket
