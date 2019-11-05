6f9d342f00db38c64ba5cf2ec450a75faacb3a85
Edmund Haselwanter <ehaselwanter@infralovers.com>
2019-11-05 13:04:19 +0100

    new version

--- install_microk8s.sh
+++ install_microk8s.sh
@@ -1,10 +1,9 @@
 
 ## microk8s
-sudo systemctl start snapd
-sudo systemctl status snapd.socket
 
 sudo snap install microk8s --channel=1.15/stable --classic
 sudo usermod -a -G microk8s centos
 sudo snap alias microk8s.kubectl kubectl
 
-microk8s.status --wait-ready
+echo "------------------------"
+echo "please log out and log in again"
--- install_snap.sh
+++ install_snap.sh
@@ -3,3 +3,9 @@ sudo yum install epel-release -y
 sudo yum install snapd -y
 sudo systemctl enable --now snapd.socket
 sudo ln -s /var/lib/snapd/snap /snap
+wait 20
+sudo systemctl start snapd
+sudo systemctl status snapd.socket
+
+echo "------------------------"
+echo "please log out and log in again"
