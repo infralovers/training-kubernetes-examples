37b0fdd47998a5324be91879908eda95521f8bac
Edmund Haselwanter <ehaselwanter@infralovers.com>
2020-04-27 16:40:59 +0200

    updated microk8s version, renaming, typo (#4)
    * updated microk8s version, renaming, typo
    
    * update to version 18
    
    * updated microk8s install script
    
    * use our new application
    
    * fix transport -> nats
    
    * add traefik config, use externalIPs
    
    Co-authored-by: Miriam Grainer <mgrainer@infralovers.com>

--- install_microk8s.sh
+++ install_microk8s.sh
@@ -1,9 +1,11 @@
-
 ## microk8s
 
-sudo snap install microk8s --channel=1.17/stable --classic
-sudo usermod -a -G microk8s centos
+sudo snap install microk8s --channel=1.18/stable --classic
+# add the current user
+sudo usermod -a -G microk8s $USER
 sudo snap alias microk8s.kubectl kubectl
 
 echo "------------------------"
 echo "please log out and log in again"
+echo "don't forget to enable needed features"
+echo " e.g. microk8s.enable dns storage"
