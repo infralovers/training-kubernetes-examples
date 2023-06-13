285eed65423b238e273b380717af1cd1090c507f
Jürgen Brüder <juergen.brueder@gmail.com>
2023-06-13 13:14:47 +0200

    feat: Replace domain placeholder

--- replace_variables.sh
+++ replace_variables.sh
@@ -8,3 +8,6 @@ find 12* deployment* handson* -type f -exec sed -i -e "s/<ENV-ANIMAL>/$ANIMAL/g"
 
 # Replace <ENV-NAME>
 find 12* deployment* handson* -type f -exec sed -i -e "s/<ENV-NAME>/$ENVIRONMENT/g" {} \;
+
+# Replace <ENV-DOMAIN>
+find 12* deployment* handson* -type f -exec sed -i -e "s/<ENV-DOMAIN>/$DOMAIN/g" {} \;
